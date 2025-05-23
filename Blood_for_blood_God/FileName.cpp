eb10970e2250b57185834e393269ee4f3703746f
Vitka999 <tsukanova1418@exemple.com>
2025-05-23 21:21:44 +0300

    функции чуть чуть доделать,отличная работа!

--- Blood_for_blood_God/FileName.cpp
+++ Blood_for_blood_God/FileName.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include "Func.h"
 using namespace std;
+
+const int MAX_TEXT = 2000;
+const int MAX_SONGS = 10;
+
 struct Music
 {
     char title[100];
     char author[100];
     int year;
-    char* text;
+    char text[MAX_TEXT];
 };
 void displayMenu() 
 {
     cout << "\nSong Lyrics Catalog\n";
-    cout << "1. Add song (from keyboard)\n";
+    cout << "1. Add song (from keyboard,from file)\n";
     cout << "2. Delete song\n";
     cout << "3. Change song text\n";
     cout << "4. Show text\n";
@@ -20,79 +24,159 @@ void displayMenu()
     cout << "0. Log out\n";
     cout << "Select an option: ";
 }
-void enterMusic(Music* music) //для ввода текста.Потом перенесёшь в Heder File
+void enterMusic(Music music[], int& songCount) 
 {
+    if (songCount >= MAX_SONGS) 
+    {
+        cout << "Catalog is full!\n";
+        return;
+    }
+
     cout << "Enter title: ";
-    cin >> music->title;
+    cin.ignore();
+    cin.getline(music[songCount].title, 100);
+
 
     cout << "Enter author: ";
-    cin >> music->author;
+    cin.getline(music[songCount].author, 100);
+
 
     cout << "Enter year: ";
-    cin >> music->year;
+    cin >> music[songCount].year;
+
+
+    cout << "Chouse the mode of entering text(1-by hand,2-from file): ";
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cin.ignore();
+        cout << "Enter text: ";
+        cin.getline(music[songCount].text, 2000);
+    }
+    else if (n==2)
+    {
+        //файл
+    }
+    else
+    {
+        cout << "Error! Enter 1 or 2!!!";
+    }
     
-    int size = 0;
+    songCount++;
+    cout << "Song added successfully!\n";
+
+   /* int size = 0;
     const int N = 1000;
     char arr[N];
-    cout << "Enter text of song: ";
     cin >> arr;
     size = strlen(arr) + 1;
     music->text = new char[size];
-    strcpy_s(music->text, size, arr);
+    strcpy_s(music->text, size, arr);*/
 }
-void deleteMusic() 
+
+void deleteMusic(Music music[], int& songCount)
+{
+    if (songCount == 0) 
+    {
+        cout << "Catalog is empty!\n";
+        return;
+    }
+    cout << "Enter song number to delete (1-" << songCount << "): ";
+    int index;
+    cin >> index;
+    index--;
+    if (index < 0 || index >= songCount)
+    {
+        cout << "Error song number!\n";
+        return;
+    }
+    for (int i = index; i < songCount - 1; i++) 
+    {
+        music[i] = music[i + 1];
+    }
+    songCount--;
+    cout << "Song deleted!\n";
+}
+void changeMusic(Music music[], int& songCount) //!
+{
+    if (songCount == 0) 
+    {
+        cout << "Catalog is empty!\n";
+        return;
+    }
+
+}
+void showText(Music music[],int songCount)
+{
+    if (songCount == 0) 
+    {
+        cout << "Catalog is empty!\n";
+        return;
+    }
+    cout << "Enter song number to show text (1-" << songCount << "): ";
+    int index;
+    cin >> index;
+    index--;
+
+    if (index < 0 || index >= songCount) 
+    {
+        cout << "Error song number!\n";
+        return;
+    }
+    else
+    {
+        cout << "\nText of song '" << music[index].title << "':\n";
+        cout << music[index].text << endl;
+    }
+
+}
+void searchMusic() //!
 {
 
 }
-void changeMusic() 
+void ShowAllMusic(Music music[], int songCount)
 {
-
-}
-void showText(Music music)
-{
-
-    /*cout << "Text of song: " << music.text << endl;*/
-}
-void searchMusic()
-{
-
-}
-void ShowAllMusic(Music music)
-{
-   /* cout << "Title: " << music.title<< endl;
-    cout << "Author: " << music.author << endl;
-    cout << "Year: " << music.year << endl;*/
+    for (int i = 0; i < songCount; i++)
+    {
+        cout << i + 1 << ". Title: " << music[i].title<< "  ";
+        cout << "Author: " << music[i].author << "  ";
+        cout << "Year: " << music[i].year << endl;
+    }
 }
 int main()
 {
+    Music catalog[MAX_SONGS];
+    int songCount = 0;
+
   int choice;
    do
    {
         displayMenu();
         cin >> choice;
-       /* switch (choice)
+       switch (choice)
         {
         case 1:
-            enterMusic();
+            enterMusic(catalog, songCount);
             break;
         case 2:
-            deleteMusic;
+            deleteMusic(catalog, songCount);
             break;
         case 3:
-            changeMusic;
+            changeMusic(catalog, songCount);
             break;
         case 4:
-            showText;
+            showText(catalog, songCount);
             break;
         case 5:
             searchMusic;
             break;
         case 6:
-            ShowAllMusic;
+            ShowAllMusic(catalog, songCount);
             break;
         default:
             cout << "Error";
-        }*/
+        }
    
    } while (choice != 0);
 	return 0;
